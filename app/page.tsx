c7d0395cda28a338577393b602bb443095d17a6b
Cleign1 <ibnukhaidar@gmail.com>
2024-12-26 16:28:46 +0700

    Update header text color to white in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <h1 className="p-4">
+      <h1 className="p-4 text-white">
         Testing cuy
       </h1>
     </div>
