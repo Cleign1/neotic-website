683b3a4f34066aac6379830bfddeeb6231485b82
Cleign1 <ibnukhaidar@gmail.com>
2024-12-26 16:28:52 +0700

    Refactor Header component to improve layout and add navigation links

--- app/header/header.tsx
+++ app/header/header.tsx
@@ -1,9 +1,59 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <header className="bg-gray-800 text-white p-4">
-      <h1>
-        Testing cuy header
-      </h1>
+    <header className="bg-white shadow-md">
+      <div className="container mx-auto px-6 py-3 flex justify-between">
+        <div className="">
+          <Link href="/" className="text-black text-xl font-bold">NEOTIC</Link>
+        </div>
+        <div className="flex ">
+          <nav>
+            <ul>
+              <li>
+                <Link
+                  href="/berita"
+                  className="text-gray-600 hover:text-gray-800 px-3 py-2 rounded">
+                  Berita
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <nav>
+            <ul>
+              <li>
+                <Link
+                  href="/portofolio"
+                  className="text-gray-600 hover:text-gray-800 px-3 py-2 rounded">
+                  Portofolio
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <nav>
+            <ul>
+              <li>
+                <Link
+                  href="/get-in-touch"
+                  className="text-gray-600 hover:text-gray-800 px-3 py-2 rounded">
+                    Get In Touch
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <nav>
+            <ul>
+              <li>
+                <Link
+                  href="/tentang"
+                  className="text-gray-600 hover:text-gray-800 px-3 py-2 rounded">
+                  Tentang
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </header>
   );
 }
