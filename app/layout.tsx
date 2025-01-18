a02c929b4ce99a3db2749c59e82cdab39a35fd06
Cleign1 <ibnukhaidar@gmail.com>
2025-01-19 00:56:06 +0700

    Replace Geist font with Inter font in layout and update class references

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,25 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
+// import { Geist } from 'next/font/google'
 import "./globals.css";
 
 import Header from "./components/header";
 import Footer from "./components/footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const Interfont  = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
-});
+})
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Neotic Website",
@@ -28,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-b from-[#2F70A1] to-[#8D8E79]`}
+        className={`${Interfont.variable} ${Interfont.variable} antialiased bg-gradient-to-b from-[#2F70A1] to-[#8D8E79]`}
       >
         <Header />
         {children}
