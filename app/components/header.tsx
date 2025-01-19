cf1633598d650cb73ae728fd7b797d6e7f17c1e1
Cleign1 <ibnukhaidar@gmail.com>
2025-01-19 17:19:29 +0700

    Refactor Header component for improved layout and mobile responsiveness

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,57 +1,99 @@
 import Link from "next/link";
 import Image from "next/image";
 import neoticText from "../components/logo/Neotic Text.svg";
+// import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
+// import { Button } from "@/components/ui/button";
 
 export default function Header() {
+
   return (
     <header className="bg-white shadow-md">
-      <div className="container mx-auto px-3 py-0 flex justify-between items-center">
+      <div className="container mx-auto px-3 py-4 flex justify-between items-center">
         <div className="flex-shrink-0">
           <Link href="/">
-            <Image
-              priority
-              src={neoticText}
-              alt="Neotic Logo"
-              width={80}
-             />
+            <Image priority src={neoticText} alt="Neotic Logo" width={80} />
           </Link>
         </div>
-        <nav className="flex-grow">
+        {/* pc navbar */}
+        <nav className="hidden md:flex flex-grow justify-center">
           <ul className="flex justify-center space-x-10">
             <li>
               <Link
                 href="/berita"
-                className="text-black text-lg font-semibold hover:text-gray-800 px-3 py-1">
+                className="text-black text-lg font-semibold hover:text-gray-800 px-3 py-1"
+              >
                 Berita
               </Link>
             </li>
             <li>
               <Link
                 href="/portofolio"
-                className="text-black text-lg font-semibold hover:text-gray-800 px-3 py-1">
+                className="text-black text-lg font-semibold hover:text-gray-800 px-3 py-1"
+              >
                 Portofolio
               </Link>
             </li>
             <li>
               <Link
                 href="https://google.com"
-                className="text-black text-lg font-semibold hover:text-gray-800 px-3 py-1">
+                className="text-black text-lg font-semibold hover:text-gray-800 px-3 py-1"
+              >
                 Get In Touch
               </Link>
             </li>
             <li>
               <Link
                 href="/tentang"
-                className="text-black text-lg font-semibold hover:text-gray-800 px-3 py-1">
+                className="text-black text-lg font-semibold hover:text-gray-800 px-3 py-1"
+              >
                 About
               </Link>
             </li>
           </ul>
         </nav>
-        <div className="flex-shrink-0 ">
+        <div className="hidden md:flex flex-shrink-0 ">
           <button className="text-black hover:text-gray-800">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+          </button>
+        </div>
+
+        {/* mobile navbar */}
+        <div className="md:hidden">
+          <button className="hover:text-gray-800">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          </button>
+        </div>
+
+        <div className="hidden md:blockflex-shrink-0 ">
+          <button className="text-black hover:text-gray-800">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+              />
             </svg>
           </button>
         </div>
