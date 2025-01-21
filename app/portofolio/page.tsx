b941691696d380c85e28b28f3dc0221c8a6baf4a
Cleign1 <ibnukhaidar@gmail.com>
2025-01-21 19:30:18 +0700

    Add PortofolioPage component with initial layout and content

---
+++ app/portofolio/page.tsx
@@ -0,0 +1,14 @@
+import { JSX } from "react/jsx-runtime"
+
+export default function PortofolioPage(): JSX.Element {
+    return(
+        <div className="my-10">
+            <div className="bg-blue-210 text-center font-semibold text-2xl p-4">
+                <h1>Portofolio</h1>
+            </div>
+            <div className="bg-blue-210 text-center font-semibold text-2xl p-4 my-10">
+                <p>testing</p>
+            </div>
+        </div>
+    )
+}
