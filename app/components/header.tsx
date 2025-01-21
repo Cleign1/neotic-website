98ce75330e5bbeee9bd130c2a06cc0622e178b42
Cleign1 <ibnukhaidar@gmail.com>
2025-01-21 15:59:54 +0700

    Remove Program link from Header component for updated navigation

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -41,12 +41,6 @@ export default function Header() {
                 Berita
               </Link>
               <Link
-                href="/program"
-                className="text-black text-base font-semibold hover:text-gray-800 px-3 py-1"
-              >
-                Program
-              </Link>
-              <Link
                 href="/portofolio"
                 className="text-black text-base font-semibold hover:text-gray-800 px-3 py-1"
               >
@@ -83,14 +77,6 @@ export default function Header() {
                 className="text-black text-lg font-semibold hover:text-gray-800 px-3 py-1"
               >
                 Berita
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/program"
-                className="text-black text-lg font-semibold hover:text-gray-800 px-3 py-1"
-              >
-                Program
               </Link>
             </li>
             <li>
