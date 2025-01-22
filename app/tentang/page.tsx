df841df6395ba99223fb037f6a3caf6cbe4d1294
Cleign1 <ibnukhaidar@gmail.com>
2025-01-22 20:28:07 +0700

    Add AboutUs component with initial layout and content

---
+++ app/tentang/page.tsx
@@ -0,0 +1,14 @@
+export default function AboutUs() {
+  return (
+    <div className="min-h-screen">
+      <div className="grid grid-flow-row auto-rows-max mt-10 mb-10 gap-10">
+        <div className="bg-blue-210 text-center font-semibold text-2xl p-4">
+            <h1>Tentang Kami </h1>
+        </div>
+        <div className="bg-blue-210 text-center font-semibold text-2xl p-4">
+            <h1>apa pulak ni</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
