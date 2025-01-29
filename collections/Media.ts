675220fdf25e127b3c91726edfcf334523802358
Cleign1 <ibnukhaidar@gmail.com>
2025-01-30 01:34:20 +0700

    Update Media.ts

--- collections/Media.ts
+++ collections/Media.ts
@@ -2,6 +2,9 @@ import type { CollectionConfig } from 'payload'
 
 export const Media: CollectionConfig = {
   slug: 'media',
+  admin: {
+    description: 'Media yang digunakan di website',
+  },
   access: {
     read: () => true,
   },
