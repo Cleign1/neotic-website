7513b97ed88cb9b9ebbf77f756009666ab2a65e8
Cleign1 <ibnukhaidar@gmail.com>
2025-01-29 23:54:17 +0700

    Add AdminLayout component for admin page structure

---
+++ app/admin/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function AdminLayout({
+    children,
+} : {
+    children: React.ReactNode
+}) {
+    return (
+        <div>
+            <h1>Admin</h1>
+            {children}
+        </div>
+    )
+}
