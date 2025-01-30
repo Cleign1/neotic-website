a4dbd2afea205168adcf6a20e4f0dfe232930d79
Cleign1 <ibnukhaidar@gmail.com>
2025-01-30 13:10:07 +0700

    Enhance Users collection: add access control and new fields for name and role

--- collections/Users.ts
+++ collections/Users.ts
@@ -1,13 +1,49 @@
 import type { CollectionConfig } from 'payload'
+import { authenticated } from '@/lib/access/authenticated'
 
 export const Users: CollectionConfig = {
   slug: 'users',
+  access: {
+    admin: authenticated,
+    create: authenticated,
+    delete: authenticated,
+    read: authenticated,
+    update: authenticated,
+  },
   admin: {
-    useAsTitle: 'email',
+    defaultColumns: ['name', 'email'],
+    useAsTitle: 'name',
   },
   auth: true,
   fields: [
-    // Email added by default
-    // Add more fields as needed
+    {
+      name: 'name',
+      label: 'Name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'email',
+      label: 'Email',
+      type: 'email',
+      required: true,
+      unique: true,
+    },
+    {
+      name: 'role',
+      label: 'Role',
+      type: 'select',
+      required: true,
+      options: [
+        {
+          label: 'Admin',
+          value: 'admin',
+        },
+        {
+          label: 'Editor',
+          value: 'Editor',
+        },
+      ],
+    }
   ],
 }
