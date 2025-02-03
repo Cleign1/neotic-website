91dd634a61380926884f796941d73e75c90506fa
Cleign1 <ibnukhaidar@gmail.com>
2025-02-03 19:00:29 +0700

    Refactor import paths for Header component to improve code organization

--- app/(frontend)/layout.tsx
+++ app/(frontend)/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 // import { Geist } from 'next/font/google'
 import "@/app/globals.css";
 
-import Header from "@/app/(frontend)/header";
+import Header from "@/components/header";
 import Footer from "@/components/footer";
 
 
