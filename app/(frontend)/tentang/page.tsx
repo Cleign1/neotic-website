92bbf4ba9f772a988bf02b5ebf94e621871da352
Cleign1 <ibnukhaidar@gmail.com>
2025-02-16 01:45:32 +0700

    Refactor AboutUs component to fetch data from Payload CMS and simplify content structure

--- app/(frontend)/tentang/page.tsx
+++ app/(frontend)/tentang/page.tsx
@@ -1,48 +1,39 @@
 import { JSX } from "react/jsx-runtime";
+import { AboutPage } from "@/payload-types";
+import { getPayload } from "payload";
+import configPromise from "@payload-config";
+import { RichText } from "@payloadcms/richtext-lexical/react";
 
-interface Section {
-  id: number;
-  title: string;
-  content: string;
+
+async function getAboutPage():Promise<AboutPage> {
+  try {
+    const payload = await getPayload({ config: configPromise });
+    const result = await payload.find({
+      collection: 'aboutPage',
+      pagination: false
+    });
+
+    // Assuming the result contains the data you need
+    const doc = result.docs[0] as AboutPage; // Return the first document in the collection
+    return {
+      id: doc.id,
+      updatedAt: doc.updatedAt,
+      createdAt: doc.createdAt,
+      ourPhilosophy: doc.ourPhilosophy,
+      whoWeAre: doc.whoWeAre,
+      whatWeDo: doc.whatWeDo,
+      ourVision: doc.ourVision,
+      ourMission: doc.ourMission
+    };
+
+  } catch (error) {
+    console.error("Error fetching data from Payload CMS:", error);
+    throw error; // Rethrow the error to handle it in the component
+  }
 }
 
-interface SectionList {
-  id: number;
-  title: string;
-  content: string[];
-}
-
-export default function AboutUs(): JSX.Element {
-  const about1: Section[] = [
-    {
-      id: 1,
-      title: "Our Philosophy",
-      content: "Dengan fokus pada pengembangan sumber daya manusia yang kompetitif, Neotic menggabungkan teknologi terkini dengan pendekatan pendidikan yang progresif. Produk dan layanan yang dihadirkan tidak hanya dirancang untuk memenuhi kebutuhan pasar saat ini, tetapi juga untuk mendorong transformasi dalam cara belajar dan bekerja."
-    },
-    {
-      id: 2,
-      title: "Who We Are",
-      content: "Membangun identitas digital perusahaan. Memperluas jangkauan informasi tentang Neotic. Meningkatkan visibilitas perusahaan secara online."
-    },
-    {
-      id: 3,
-      title: "What we Do",
-      content: "Menciptakan produk aplikasi desktop, website, dan mobile untuk kebutuhan konsumen dan klien"
-    }
-  ]
-
-  const about2: SectionList[] = [
-    {
-      id: 1,
-      title: "Our Vision",
-      content: ["Memberikan informasi komprehensif tentang layanan dan produk", "Membangun Kepercayaan Stakeholder", "Meningkatkan Jangkauan Pemasaran"]
-    },
-    {
-      id: 2,
-      title: "Our Mission",
-      content: ["Memudahkan calon klien/mitra mengenal perusahaan","Meningkatkan kredibilitas perusahaan", "Sebagai media promosi dan komunikasi digital"]
-    }
-  ]
+export default async function AboutUs(): Promise<JSX.Element> {
+  const aboutPageData = await getAboutPage();
 
   return (
     <div className="min-h-screen">
@@ -57,19 +48,25 @@ export default function AboutUs(): JSX.Element {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
               {/* Box 1 */}
               <div className="bg-white p-4 rounded-lg hover:shadow-lg">
-                <h1 className="text-center p-5 text-2xl font-semibold">{about1.find((s) => s.id === 1)?.title}</h1>
-                <p className="m-10 text-justify">{about1.find((s) => s.id === 1)?.content}</p>
+                <h1 className="text-center p-5 text-2xl font-semibold">Our Philosophy</h1>
+                <p className="m-10 text-justify">
+                  {aboutPageData.ourPhilosophy} 
+                  </p>
               </div>
               <div className="grid grid-row-2 gap-10">
                 {/* Box 2 */}
                 <div className="bg-white p-4 rounded-lg hover:shadow-lg">
-                  <h1 className="text-center text-2xl font-semibold p-4">{about1.find((s) => s.id === 2)?.title}</h1>
-                  <p className="m-10 text-justify">{about1.find((s) => s.id === 2)?.content}</p>
+                  <h1 className="text-center text-2xl font-semibold p-4">Who We Are</h1>
+                  <p className="m-10 text-justify">
+                    {aboutPageData.whoWeAre} 
+                  </p>
                 </div>
                 {/* Box 3 */}
                 <div className="bg-white p-4 rounded-lg hover:shadow-lg">
-                  <h1 className="text-center text-2xl p-4 font-semibold">{about1.find((s) => s.id === 3)?.title}</h1>
-                  <p className="m-10 text-justify">{about1.find((s) => s.id === 3)?.content}</p>
+                  <h1 className="text-center text-2xl p-4 font-semibold">What We Do</h1>
+                  <p className="m-10 text-justify">
+                    {aboutPageData.whatWeDo} 
+                  </p>
                 </div>
               </div>
             </div>
@@ -80,18 +77,19 @@ export default function AboutUs(): JSX.Element {
         <div className="bg-blue-210 text-center p-4">
           <div className="container mx-auto m-6 px-3 md:px-7">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
-              {about2.map((section) => (
-                <div key={section.id} className="bg-white p-4 rounded-lg hover:shadow-lg">
-                <h1 className="text-2xl p-4 font-semibold">{section.title}</h1>
-                <ul className="list-disc m-10 pl-5">
-                  {section.content.map((item, index) => (
-                    <li key={index} className="text-justify">
-                      {item}
-                    </li>
-                  ))}
-                </ul>
+              <div className="bg-white p-4 rounded-lg hover:shadow-lg">
+                <h1 className="text-2xl p-4 font-semibold">Our Vision</h1>
+                <div className="m-10 text-justify">
+                  {/* Debug ini coy, masih gk jelas */}
+                  {aboutPageData.ourVision && <RichText data={aboutPageData.ourVision}/>}
+                </div>
               </div>
-              ))}
+              <div className="bg-white p-4 rounded-lg hover:shadow-lg">
+                <h1 className="text-2xl p-4 font-semibold">Our Mission</h1>
+                <div className="m-10 text-justify">
+                {aboutPageData.ourMission && <RichText data={aboutPageData.ourMission}/>}
+                </div>
+              </div>
             </div>
           </div>
         </div>
