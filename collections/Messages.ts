0e13282b909c618a7ae8c5e502bdd33a004a598b
Cleign1 <ibnukhaidar@gmail.com>
2025-02-17 20:52:34 +0700

    Add Messages collection configuration with fields for user input

---
+++ collections/Messages.ts
@@ -0,0 +1,34 @@
+import { CollectionConfig } from "payload";
+
+export const Messages: CollectionConfig = {
+    slug: "messages",
+    fields: [
+        {
+            name: "name",
+            type: "text",
+            required: true,
+        },
+        {
+            name: "email",
+            type: "email",
+            required: true,
+        },
+        {
+            name: "phone",
+            type: "text",
+        },
+        {
+            name: 'messageTitle',
+            type: 'text',
+            required: true,
+        },
+        {
+            name: "message",
+            type: "textarea",
+            required: true,
+        }
+    ],
+    admin: {
+        useAsTitle: 'messageTitle',
+    }
+}
