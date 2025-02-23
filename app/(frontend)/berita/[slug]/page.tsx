d5e15d0976ef1a4bbc13f0919c844580040a81d6
Cleign1 <ibnukhaidar@gmail.com>
2025-02-23 18:51:19 +0700

    Refactor BeritaPage to use NotFound component for 404 error handling

--- app/(frontend)/berita/[slug]/page.tsx
+++ app/(frontend)/berita/[slug]/page.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { KontenBerita } from "@/payload-types"; // Adjust the import path as needed
 import { RichText } from "@payloadcms/richtext-lexical/react";
 import { SerializedEditorState } from "@payloadcms/richtext-lexical/lexical";
+import NotFound from "../../[...notFound]/page";
 
 interface BeritaPage {
   id: string;
@@ -80,18 +81,7 @@ export default async function BeritaPage({
 
   if (!berita) {
     return (
-      <div className="my-10 min-h-screen">
-      <div className="bg-blue-210 text-center font-semibold p-4">
-          <Link href='/berita'>
-          <h1 className="text-2xl md:text-3xl">Berita</h1>
-          </Link>
-      </div>
-      <div className="bg-blue-210 p-4 md:p-8 mt-10 mb-10">
-          <div className="container mx-auto py-10 md:py-20">
-          <h1 className="text-xl md:text-2xl font-semibold p-4 md:p-6 text-center">Halaman tidak ditemukan</h1>
-          </div>
-      </div>
-      </div>
+      <NotFound />
   );
   }
 
